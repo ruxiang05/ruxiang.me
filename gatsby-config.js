6e18e3ec2a8adb68c351178c1d914f3bd27586fb
Ruxandra Anghel <ruxiang05@gmail.com>
2018-08-15 21:58:23 +0100

    Add React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
