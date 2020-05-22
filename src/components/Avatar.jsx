b40950cb3e561fe70185b6ca937bb439c00167c7
Ruxandra Anghel <ruxiang05@gmail.com>
2020-05-22 14:11:42 +0100

    Add new website

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import avatar from "../images/avatar.jpg"
+
+const Avatar = () => (
+  <img className="profile" src={avatar} alt="Ruxandra Anghel's profile" />
+)
+
+export default Avatar
