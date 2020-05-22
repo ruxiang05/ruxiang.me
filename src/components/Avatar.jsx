0f3b354d52aaba9cc2ca3a875f5a1c9e376123b8
Ruxandra Anghel <ruxiang05@gmail.com>
2020-05-22 15:30:57 +0100

    Optimise image

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,26 @@
 import React from "react"
-import avatar from "../images/avatar.jpg"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Avatar = () => (
-  <img className="profile" src={avatar} alt="Ruxandra Anghel's profile" />
-)
+const Avatar = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "avatar.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1080) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
+  return (
+    <Img
+      className="profile"
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      objectFit="cover"
+    />
+  )
+}
 export default Avatar
